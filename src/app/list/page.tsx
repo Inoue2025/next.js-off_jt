dcaaee9da399d4a4cb9c71f6fd289098009c15e7
Inoue2025 <sumire.inoue@metateam.co.jp>
2025-05-26 12:36:16 +0900

    layout.tsx の演習を行いました

---
+++ src/app/list/page.tsx
@@ -0,0 +1,12 @@
+export default function ListPage() {
+	return (
+		<ul>
+			<li>ぐりとぐら</li>
+			<li>はらぺこあおむし</li>
+			<li>ねずみくんのチョッキ</li>
+			<li>のんたん</li>
+			<li>ぶたのたね</li>
+			<li>しろくまちゃんのほっとけーき</li>
+		</ul>
+	);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-export default function Home() {
-	const name: string = "Yuki";
-
-	return (
-		<main>
-			<h1>こんにちは、{name}さん</h1>
-		</main>
-	);
+export default function HomePage() {
+	return <h1>こんにちは</h1>;
 }
