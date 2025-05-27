7e493cedaa51c8590b8956c9ad6d411979cddce6
Inoue2025 <sumire.inoue@metateam.co.jp>
2025-05-27 10:50:38 +0900

    インデントの種類をスペースに変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,37 +2,37 @@ import "./globals.css";
 import Link from "next/link";
 
 export const metadata = {
-	title: "こんにちは",
+  title: "こんにちは",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: { children: React.ReactNode }) {
-	return (
-		<html lang="ja">
-			<body className="flex flex-col min-h-screen">
-				<header className="bg-gray-200 text-center p-4">
-					<nav>
-						<ul>
-							<li>
-								<Link href="/">home</Link>
-							</li>
-							<li>
-								<Link href="/top">top</Link>
-							</li>
-							<li>
-								<Link href="/list">list</Link>
-							</li>
-						</ul>
-					</nav>
-				</header>
+  return (
+    <html lang="ja">
+      <body className="flex flex-col min-h-screen">
+        <header className="bg-gray-200 text-center p-4">
+          <nav>
+            <ul>
+              <li>
+                <Link href="/">home</Link>
+              </li>
+              <li>
+                <Link href="/top">top</Link>
+              </li>
+              <li>
+                <Link href="/list">list</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
 
-				<main className="flex-1 p-4">{children}</main>
+        <main className="flex-1 p-4">{children}</main>
 
-				<footer className="bg-gray-200 text-center p-4">
-					<p>©2025 My Memo App</p>
-				</footer>
-			</body>
-		</html>
-	);
+        <footer className="bg-gray-200 text-center p-4">
+          <p>©2025 My Memo App</p>
+        </footer>
+      </body>
+    </html>
+  );
 }
--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,12 +1,12 @@
 export default function ListPage() {
-	return (
-		<ul>
-			<li>ぐりとぐら</li>
-			<li>はらぺこあおむし</li>
-			<li>ねずみくんのチョッキ</li>
-			<li>のんたん</li>
-			<li>ぶたのたね</li>
-			<li>しろくまちゃんのほっとけーき</li>
-		</ul>
-	);
+  return (
+    <ul>
+      <li>ぐりとぐら</li>
+      <li>はらぺこあおむし</li>
+      <li>ねずみくんのチョッキ</li>
+      <li>のんたん</li>
+      <li>ぶたのたね</li>
+      <li>しろくまちゃんのほっとけーき</li>
+    </ul>
+  );
 }
